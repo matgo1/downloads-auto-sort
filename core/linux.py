68d77aba3df74f0d811b080f14428fcbd034c4c4
matgo1 <matveistarostin2707@gmail.com>
2026-01-03 22:04:17 +0300

    Writing for linux

--- core/linux.py
+++ core/linux.py
@@ -7,6 +7,7 @@ from typing import Set
 class Sorter():
     HOME_DIR: str = os.path.expanduser('~') # Constant of home library
     PATH: Path = Path(f"{HOME_DIR}/Downloads")
+    DOCS_EXT = {".txt", ".doc", ".docx"}
     
     def __init__(self) -> None:
         pass
@@ -14,19 +15,20 @@ class Sorter():
     def move_to_downloads(self) -> None:
         os.chdir(self.PATH) # Move to downloads directory
         
-    def check_for_new_files(self, interval: int = 5) -> bool:
-        self.is_new_files: bool = False
+    def check_for_new_files(self) -> bool:
+        is_new_files: bool = False
         # Get current files
         initial_files: Set[str] = set(os.listdir(self.PATH))
         
-        while True:
-            time.sleep(interval)
-            #Check for new files
-            current_files: Set[str] = set(os.listdir(self.PATH))
+        #Check for new files
+        current_files: Set[str] = set(os.listdir(self.PATH))
+        self.new_files: Set[str] = initial_files - current_files
+        
+        if self.new_files != 0:
+            print("New files detected!")
+            is_new_files = True
             
-            if current_files - initial_files != 0:
-                print("New files detected!")
-                self.is_new_files = True
+        return is_new_files
 
     def make_directories(self) -> None:
         # Make all Directories
@@ -34,8 +36,15 @@ class Sorter():
             os.mkdir("Images")
         elif not "Documents" in os.listdir(self.PATH):
             os.mkdir("Documents")
+        elif not "Videos" in os.listdir(self.PATH):
+            os.mkdir("Videos")
         elif not "Other" in os.listdir(self.PATH):
             os.mkdir("Other")
         
     def sort_files(self) -> None:
-        
+        """
+        Check for extension in files in sort them
+        """
+        for f in self.new_files:
+            file: Path = Path(f)
+            if file.suffix.lower() in 
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 from core import (linux,
                   win,
                   macos)
+import time
 
 def main() -> None:
     if sys.platform == "win32": # If it is windows, start code for windows
@@ -14,8 +15,11 @@ def main() -> None:
         sorter.make_directories() # Make importatnt dirs
         
         while True:
+            time.sleep(60) # Check for new files every minute
             if sorter.check_for_new_files():
-                
+                sorter.sort_files()
+            else:
+                continue
 
     elif sys.platform == "darwin": # if it is macOS, start code for macOS
         pass
